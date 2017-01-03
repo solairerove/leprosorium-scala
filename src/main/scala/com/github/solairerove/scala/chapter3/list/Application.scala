51d7f475fb08385a83e9017b74de22f89971d03c
solairerove <hax2033@gmail.com>
2017-01-04 00:51:26 +0300

    Create simple list concatenating example: - awesome

---
+++ src/main/scala/com/github/solairerove/scala/chapter3/list/Application.scala
@@ -0,0 +1,13 @@
+package com.github.solairerove.scala.chapter3.list
+
+object Application {
+  def main(args: Array[String]): Unit = {
+    val oneTwo = List(1, 2)
+    val threeFour = List(3, 4)
+
+    println(oneTwo ::: threeFour)
+    val array = 1 :: threeFour
+
+    array.foreach(el => print(el))
+  }
+}
